19662addfe31390bbdfbe419f92e952d98c344b5
Drake Scott <dascott0204@gmail.com>
2017-03-13 19:48:47 -0500

    why generics?

--- app.ts
+++ app.ts
@@ -1,3 +1,11 @@
 /**
  * Created by dasco on 3/9/2017.
  */
+// Simple Generic
+function echo(data: any) {
+    return data;
+}
+
+console.log(echo("Drake"));
+console.log(echo(20));      //.length would not work, but typescript would not know better
+console.log(echo({name: "Drake",age: 20}));
